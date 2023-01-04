d7935fc2d434a2e3efa28d02a71a1c4ed803a7d3
Martin <martinyang29@gmail.com>
2023-01-03 19:20:23 -0500

    Fixed op control for expansion

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -5,54 +5,54 @@
 #include "lib/scoring.hpp"
 
 void opcontrol() {
-	unsigned shootingSpeed = 2000;
+    unsigned shootingSpeed = 2000;
     pros::Task changeFlywheelSpeed(regulateFlywheel, &shootingSpeed);
-	
-	expander1_piston.set_value(0);
-	expander2_piston.set_value(0);
 
-	flywheel_indexer.set_value(0);
-	int intake_state=1;
-	int flywheel_state=0;
-	bool shooterReady = false;
-	while (true) {
-		int power = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-		int turnRate = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-		move(power - turnRate, power + turnRate);
-		if(master.get_digital(DIGITAL_L1))
-		{
-			intake=-127;
-		}
-		else if(master.get_digital(DIGITAL_L2))
-		{
-			intake=0;
-		}
-		else 
-		{
-			intake=127;
-		}
-		if (master.get_digital(DIGITAL_R2) )
+    expander1_piston.set_value(0);
+    expander2_piston.set_value(0);
+
+    flywheel_indexer.set_value(0);
+    int intake_state=1;
+    int flywheel_state=0;
+    bool shooterReady = false;
+    while (true) {
+        int power = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int turnRate = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        move(power + turnRate, power - turnRate);
+        if(master.get_digital(DIGITAL_L1))
+        {
+            intake=-127;
+        }
+        else if(master.get_digital(DIGITAL_L2))
+        {
+            intake=0;
+        }
+        else 
+        {
+            intake=127;
+        }
+        if (master.get_digital(DIGITAL_R2) )
  {
-				flywheel_indexer.set_value(1);
-				pros::delay(150);
-				flywheel_indexer.set_value(0);
+                flywheel_indexer.set_value(1);
+                pros::delay(150);
+                flywheel_indexer.set_value(0);
  }
-		if (master.get_digital(DIGITAL_R1))
+        if (master.get_digital(DIGITAL_R1))
  {
-			//vector center11 = {};
-			//aim_shot(&center11);
-		}
+            //vector center11 = {};
+            //aim_shot(&center11);
+        }
 
-		if(master.get_digital(DIGITAL_UP) && master.get_digital (DIGITAL_DOWN) && master.get_digital (DIGITAL_RIGHT) && master.get_digital (DIGITAL_LEFT))
-		{
-			expander1_piston.set_value(1);
-			expander2_piston.set_value(1);
-		}
-		else
-		{
-			expander1_piston.set_value(0);
-			expander2_piston.set_value(0);
-		}
-		
-	}
+        if(master.get_digital(DIGITAL_UP) && master.get_digital (DIGITAL_DOWN) && master.get_digital (DIGITAL_RIGHT) && master.get_digital (DIGITAL_LEFT))
+        {
+            expander1_piston.set_value(1);
+            expander2_piston.set_value(1);
+        }
+        else
+        {
+            expander1_piston.set_value(0);
+            expander2_piston.set_value(0);
+        }
+
+    }
 }
